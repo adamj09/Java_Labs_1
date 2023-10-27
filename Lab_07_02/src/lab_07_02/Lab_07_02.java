40a18303872b7949f10d2c4460224b775f2e3e68
Adam Johnston <adamjohnston144@gmail.com>
2023-10-26 20:57:14 -0400

    completed

--- Lab_07_02/src/lab_07_02/Lab_07_02.java
+++ Lab_07_02/src/lab_07_02/Lab_07_02.java
@@ -4,9 +4,11 @@ import java.util.Scanner;
 
 public class Lab_07_02 {
     public static void main(String[] args) {
-        //displayGrades();
+        displayGrades();
         //displayReversedNumbers();
         //countNumberOccurence();
+        //analyzeScores();
+        //displayUniqueNumbers();
     }
 
     public static void displayGrades(){
@@ -25,7 +27,7 @@ public class Lab_07_02 {
             }
         }
         char[] grades = new char[scores.length];
-        System.arraycopy(scoresToGrades(scores), 0, grades, 0, scores.length);
+        System.arraycopy(scoresToGrades(scores), 0, grades, 0, scores.length); // copy contents from function return value
         for(int i = 0; i < scores.length; i++){
             System.out.printf("Student %d's score is %.2f and their grade is %s\n", i, scores[i], grades[i]);
         }
@@ -39,11 +41,21 @@ public class Lab_07_02 {
 
         char[] grades = new char[scores.length];
         for(int i = 0; i < scores.length; i++){ // initialize grades array with scores
-            if(scores[i] >= (bestScore - 10)) grades[i] = 'A';
-            else if(scores[i] >= (bestScore - 20)) grades[i] = 'B';
-            else if(scores[i] >= (bestScore - 30)) grades[i] = 'C';
-            else if(scores[i] >= (bestScore - 40)) grades[i] = 'D';
-            else grades[i] = 'F';
+            if(scores[i] >= (bestScore - 10)){
+                grades[i] = 'A';
+            }
+            else if(scores[i] >= (bestScore - 20)){
+                grades[i] = 'B';
+            }
+            else if(scores[i] >= (bestScore - 30)){
+                grades[i] = 'C';
+            }
+            else if(scores[i] >= (bestScore - 40)){
+                grades[i] = 'D';
+            }
+            else{
+                grades[i] = 'F';
+            }
         }
 
         return grades;
@@ -54,12 +66,14 @@ public class Lab_07_02 {
         System.out.print("Enter 10 integers: ");
         int[] original = new int[10];
         
-        for(int i = 0; i < original.length; i++){
+        for(int i = 0; i < original.length; i++){ // fill original numbers array
             original[i] = input.nextInt();
         }
 
+        // copy contents from function return value
         int[] reversed = new int[original.length];
         System.arraycopy(reverseNumbers(original), 0, reversed, 0, original.length);
+
         for(int value : reversed){
             System.out.print(value + " ");
         }
@@ -78,32 +92,29 @@ public class Lab_07_02 {
     public static void countNumberOccurence(){
         Scanner input = new Scanner(System.in);
         System.out.print("Enter integers between 1 and 100, the input ends at 0, or if there are 100 input numbers: ");
-
         int[] numbers = new int[100];
-        for(int i = 0; i < numbers.length; i++){ // put all numbers in single array
-            int currentNumber = input.nextInt();
 
-            if(currentNumber == 0) break; // stop input at 0
-
+        // Fill array with input, stop when input is 0 or array is completely full
+        for(int i = 0, currentNumber; (currentNumber = input.nextInt()) != 0 && i < numbers.length; i++){
             if(currentNumber > 100 || currentNumber < 1){ // make sure number is between 1 and 100
                 System.out.println("Invalid input: numbers must be between 1 and 100.");
-                return;
+                return; // cancel operation if input is not valid
             }
             numbers[i] = currentNumber;
         }
 
         int[] uniqueNumbers = new int[100];
-        for(int i = 0, j = 0; numbers[i] != 0; i++){ // store all the unique numbers
-            if(!containsNumber(uniqueNumbers, numbers[i])){
+        for(int i = 0, j = 0; numbers[i] != 0; i++){ // store the unique numbers in an array
+            if(!containsInt(uniqueNumbers, numbers[i])){
                 uniqueNumbers[j] = numbers[i];
                 j++;
             }
         }
 
         int[] numberCounts = new int[100]; 
-        for(int i = 0; uniqueNumbers[i] != 0; i++){ // count the occurences
+        for(int i = 0; uniqueNumbers[i] != 0; i++){ // go through all the numbers for every unique number and count their occurences
             for(int j = 0; numbers[j] != 0; j++){
-                if(numbers[j] == uniqueNumbers[i]){
+                if(numbers[j] == uniqueNumbers[i]){ // found an occurence of the current number
                     numberCounts[i]++;
                 }
             }
@@ -114,12 +125,69 @@ public class Lab_07_02 {
         }
     } // End of countNumberOccurence method
 
-    public static boolean containsNumber(int[] numbers, int number){
-        for(int num : numbers)
-            if(num == number) return true;
+    public static void analyzeScores(){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter some scores to analyze (max number of scores is 100), input ends with a negative number: ");
+        double[] scores = new double[100];
 
+        // fill array with input scores, stop at negative number or 100 inputs
+        int scoreCount = 0;
+        while(((scores[scoreCount] = input.nextDouble()) > 0) && (scoreCount < scores.length)){
+            scoreCount++;
+        }
+
+        // Compute average
+        double sum = 0;
+        for(int i = 0; scores[i] > 0; i++){
+            sum += scores[i];
+        }
+        double average = sum / scoreCount;
+
+        // Compute number of scores above/equal to or below average
+        int countAboveEqualAve = 0, countBelowAve = 0;
+        for(int i = 0; scores[i] > 0; i++){
+            if(scores[i] >= average){
+                countAboveEqualAve++;
+            }
+            else{
+                countBelowAve++;
+            }
+        }
+        
+        System.out.printf("The average score is: %.2f\nThe number of scores above or equal to the average is: %d\nThe number of scores below the average is: %d\n",
+                average, countAboveEqualAve, countBelowAve);
+    } // End of analyzeScores method
+
+    public static void displayUniqueNumbers(){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter 10 integers: ");
+        int[] numbers = new int[10];
+        
+        for(int i = 0; i < numbers.length; i++){ // fill original numbers array
+            numbers[i] = input.nextInt();
+        }
+
+        int[] uniqueNumbers = new int[numbers.length];
+        int uniqueCount = 0;
+        for(int i = 0; i < numbers.length; i++){ // store the unique numbers in an array and keep count
+            if(!containsInt(uniqueNumbers, numbers[i])){
+                uniqueNumbers[uniqueCount] = numbers[i];
+                uniqueCount++;
+            }
+        }
+
+        System.out.print("The number of unique numbers is: " + uniqueCount + "\nThe unique numbers are: ");
+        for(int i = 0; uniqueNumbers[i] != 0; i++){
+            System.out.print(uniqueNumbers[i] + " ");
+        }
+    } // End of displayUniqueNumbers method
+
+    public static boolean containsInt(int[] numbers, int number){
+        for(int num : numbers){
+            if(num == number){ 
+                return true;
+            }
+        }
         return false;
-    } // End of containsNumber method
-
-    
+    } // End of arrayContainsInteger method
 }
