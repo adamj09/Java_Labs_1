4b824a0bcd9f42c5c354f023883d2a0a76ddcb46
Adam Johnston <adamjohnston144@gmail.com>
2023-11-02 16:56:09 -0400

    done question 2

--- Lab_08_02/src/lab_08_02/Lab_08_02.java
+++ Lab_08_02/src/lab_08_02/Lab_08_02.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 public class Lab_08_02 {
     public static void main(String[] args) {
-        //System.out.println("Random number (excluding 5, 6, and 8): " + getRandom(5, 6, 8));
-        System.out.println(gcd(8, 20));
+        //System.out.println("Random number (excluding 5, 6, and 8): " + getRandom(5, 6, 8)); // Question 1
+        gcdTest();
     }
 
     public static int getRandom(int... excluded){
@@ -28,25 +28,39 @@ public class Lab_08_02 {
     } // end of getRandom method
 
     public static int gcd(int... numbers){
-        int gcd = Math.min(numbers[0], numbers[1]);
+        if(numbers.length == 1){
+            return numbers[0]; // return first number in list if only one number is given
+        }
 
-        // find the GDC of the first pair of numbers
+        // find the GCD of the first pair of numbers (we use abs so this works with negatives too)
+        int gcd = Math.abs(Math.min(numbers[0], numbers[1]));
         while(numbers[0] % gcd != 0 || numbers[1] % gcd != 0){
             gcd--;
         }
-
         int previousGcd = gcd;
-        for(int i = 2; i < numbers.length; i++){ // find the GDC of the previous GDC and the next number
-            gcd = Math.min(previousGcd, numbers[i]);
-            while(previousGcd % gcd != 0 || numbers[i] % gcd != 0){
+
+        for(int i = 2; i < numbers.length; i++){
+            gcd = Math.abs(Math.min(previousGcd, numbers[i])); // init new start value for GCD
+            while(previousGcd % gcd != 0 || numbers[i] % gcd != 0){ // find the GCD of the previous GCD and the next number
                 gcd--;
             }
 
-            if(gcd == 1){
+            if(gcd == 1){ // if GCD is 1, no point in continuing, so return GCD early
                 return gcd;
             }
             previousGcd = gcd;
         }
         return gcd;
+    } // end of gcd method
+
+    public static void gcdTest(){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter 5 numbers: ");
+        
+        int[] numbers = new int[5];
+        for(int i = 0; i < numbers.length; i++){
+            numbers[i] = input.nextInt();
+        }
+        System.out.printf("The GCD of these numbers is: %d", gcd(numbers));
     }
 }
