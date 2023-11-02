3f13ae2af818fd206e93d82e4327f23e6c7457b2
AdamJ <adamjohnston144@gmail.com>
2023-11-02 11:58:28 -0400

    question 1

--- Lab_08_02/src/lab_08_02/Lab_08_02.java
+++ Lab_08_02/src/lab_08_02/Lab_08_02.java
@@ -4,6 +4,31 @@ import java.util.Scanner;
 
 public class Lab_08_02 {
     public static void main(String[] args) {
-        
+        System.out.println("Random number (excluding 5, 6, and 8): " + getRandom(5, 6, 8));
+    }
+
+    public static int getRandom(int... excluded){
+        boolean containsExcluded = false;
+        int randNum;
+
+        do {
+            randNum = (int)(Math.random() * 54 + 1); // generate randNum
+
+            for(int number : excluded){ // check if randNum is one of the excluded numbers
+                if(randNum == number){
+                    containsExcluded = true;
+                    break;
+                }
+                containsExcluded = false;
+            }
+        } while(containsExcluded == true); // if randomNum is in list of excluded numbers, try again
+
+        return randNum;
+    } // end of getRandom method
+
+    public static int gcd(int... numbers){
+        for(int number : numbers)}{
+            
+        }
     }
 }
