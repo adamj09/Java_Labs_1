d4af19176095ba37cd75544b23276445e29ec003
AdamJ <adamjohnston144@gmail.com>
2023-11-02 13:48:22 -0400

    question 2

--- Lab_08_02/src/lab_08_02/Lab_08_02.java
+++ Lab_08_02/src/lab_08_02/Lab_08_02.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 
 public class Lab_08_02 {
     public static void main(String[] args) {
-        System.out.println("Random number (excluding 5, 6, and 8): " + getRandom(5, 6, 8));
+        //System.out.println("Random number (excluding 5, 6, and 8): " + getRandom(5, 6, 8));
+        System.out.println(gcd(8, 20));
     }
 
     public static int getRandom(int... excluded){
@@ -27,8 +28,26 @@ public class Lab_08_02 {
     } // end of getRandom method
 
     public static int gcd(int... numbers){
-        for(int number : numbers)}{
-            
+        int gcd = Math.min(numbers[0], numbers[1]);
+
+        // find the GDC of the first pair of numbers
+        while(numbers[0] % gcd != 0 || numbers[1] % gcd != 0){
+            gcd--;
         }
+
+        int previousGcd = gcd;
+
+        for(int i = 2; i < numbers.length; i++){ // find the GDC of the previous GDC and the next number
+            gcd = Math.min(previousGcd, numbers[i]);
+            while(previousGcd % gcd != 0 || numbers[i] % gcd != 0){
+                gcd--;
+            }
+
+            if(gcd == 1){
+                return gcd;
+            }
+            previousGcd = gcd;
+        }
+        return gcd;
     }
 }
