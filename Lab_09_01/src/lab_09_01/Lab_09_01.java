c5cac595c690e5480fd3a8c245130910bd270fce
AdamJ <adamjohnston144@gmail.com>
2023-11-06 13:33:40 -0500

    question 1

--- Lab_09_01/src/lab_09_01/Lab_09_01.java
+++ Lab_09_01/src/lab_09_01/Lab_09_01.java
@@ -4,6 +4,68 @@ import java.util.Scanner;
 
 public class Lab_09_01 {
     public static void main(String[] args) {
+        //hasFourConsecutiveTest(); // Question 1
 
     }
+
+    public static boolean hasFourConsecutive(int... numbers){
+        int previousNumber = numbers[0], count = 1;
+        for(int i = 1; i < numbers.length; i++){
+            if(numbers[i] == previousNumber){
+                if(++count == 4){
+                    return true;
+                }
+            }
+            else{
+                count = 1;
+            }
+            previousNumber = numbers[i];
+        }
+        return false;
+    } // end of hasFourConsecutive method
+
+    public static void hasFourConsecutiveTest(){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number of integers: ");
+        int[] values = new int[input.nextInt()];
+
+        System.out.print("Enter the integers: ");
+        for(int i = 0; i < values.length; i++){
+            values[i] = input.nextInt();
+        }
+        System.out.printf("This array %s four consecutive numbers.", hasFourConsecutive(values) ? "has": "does not have");
+    } // end of hasFourConsecutiveTest method
+
+    public static int[] mergeSorted(int[] array1, int[] array2){
+        array1 = selectSortInc(array1);
+        array2 = selectSortInc(array2);
+
+        int newLength = array1.length + array2.length;
+        int[] merged = new int[newLength];
+
+        for(int i = 0; i < newLength; i++){
+            
+        }
+
+        return merged;
+    } // end of merge method
+
+    public static int[] selectSortInc(int[] array){
+        int[] sorted = new int[array.length];
+        for(int i = 0; i < array.length; i++){
+            // find index of minimum value from i onwards
+            int minIndex = i;
+            for(int j = i; j < array.length; j++){
+                if(array[minIndex] > array[j]){
+                    minIndex = j;
+                }
+            }
+
+            // add min to sorted and swap number in array
+            int temp = array[i];
+            sorted[i] = array[minIndex];
+            array[minIndex] = temp;
+        }
+        return sorted;
+    } // end of selectSortInc method
 }
