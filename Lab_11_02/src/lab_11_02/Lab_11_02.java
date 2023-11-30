e3e4ddbeec37ff0f6781af5ab9d3bccae6c94793
AdamJ <adamjohnston144@gmail.com>
2023-11-30 13:52:56 -0500

    Q2

--- Lab_11_02/src/lab_11_02/Lab_11_02.java
+++ Lab_11_02/src/lab_11_02/Lab_11_02.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 
 public class Lab_11_02 {
     public static void main(String[] args) {
-        locationDemo();
+        //locationDemo();
+        linearEquationDemo();
     }
 
     public static void locationDemo(){
@@ -22,5 +23,16 @@ public class Lab_11_02 {
 
         Location location = Location.locateLargest(array);
         System.out.printf("The largest element is %.2f, located at (%d, %d).", location.maxValue, location.row, location.column);
+    }
+    
+    public static void linearEquationDemo(){
+        //LinearEquation equation = new LinearEquation(3.4, 50.2, 44.5, 2.1, 0.55, 5.9);
+        LinearEquation equation = new LinearEquation(1, 2, 2, 4, 4, 5);
+        if(!equation.isSolvable()){
+            System.out.print("This equation has no solutions.\n");
+        }
+        else{
+            System.out.printf("The solution for x is %.2f, the solution for y is %.2f\n", equation.getX(), equation.getY());
+        }
     }
 }
