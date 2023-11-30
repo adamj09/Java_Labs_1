d91781d613d193165230c59243c6fd31cfdd47f5
AdamJ <adamjohnston144@gmail.com>
2023-11-30 13:12:43 -0500

    finished Q1

--- Lab_11_02/src/lab_11_02/Lab_11_02.java
+++ Lab_11_02/src/lab_11_02/Lab_11_02.java
@@ -1,7 +1,26 @@
 package lab_11_02;
 
+import java.util.Scanner;
+
 public class Lab_11_02 {
     public static void main(String[] args) {
+        locationDemo();
+    }
 
+    public static void locationDemo(){
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the matrix's number of rows and columns respectively: ");
+        double[][] array = new double[input.nextInt()][input.nextInt()];
+
+        System.out.println("Enter the matrix row by row: ");
+        for(int i = 0; i < array.length; i++){
+            for(int j = 0; j < array[i].length; j++){
+                array[i][j] = input.nextDouble();
+            }
+        }
+
+        Location location = Location.locateLargest(array);
+        System.out.printf("The largest element is %.2f, located at (%d, %d).", location.maxValue, location.row, location.column);
     }
 }
--- Lab_11_02/src/lab_11_02/Location.java
+++ Lab_11_02/src/lab_11_02/Location.java
@@ -1,8 +1,27 @@
+package lab_11_02;
+
 public class Location {
     public int row, column;
     public double maxValue;
 
+    Location(int row, int column, double maxValue){
+        this.row = row;
+        this.column = column;
+        this.maxValue = maxValue;
+    }
+
     public static Location locateLargest(double[][] array){
-        
+        double maxValue = array[0][0];
+        int row = 0, column = 0;
+        for(int i = 0; i < array.length; i++){
+            for(int j = 0; j < array[i].length; j++){
+                if(array[i][j] > array[row][column]){
+                    row = i;
+                    column = j;
+                    maxValue = array[row][column];
+                }
+            }
+        }
+        return new Location(row, column, maxValue);
     }
 }
